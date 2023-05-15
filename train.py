b985b3fc205c207f6f08dce626cec3266f0f0a5a
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2023-05-14 19:32:46 -0700

    Add Apache License

--- train.py
+++ train.py
@@ -1,7 +1,22 @@
 # train.py
+
 # Trains and tests an index on the works embeddings, yielding a trained index 
 #  and a set of Pareto-optimal pairs of recall (10@10) scores and time taken for 
 #  that index.
+
+# Copyright 2023 Kenny Peng
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
 import numpy as np
