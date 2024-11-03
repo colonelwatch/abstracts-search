a466d8df260b043a4ce839cded8dbffee2711784
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2024-11-03 12:35:11 -0800

    Reorganize encode.py

--- encode.py
+++ encode.py
@@ -14,8 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from argparse import ArgumentParser, Namespace
 import os
-import sys
+
 import pyarrow as pa
 import pyarrow.parquet as pq
 import pyarrow.dataset as ds
@@ -23,7 +24,17 @@ import pyarrow.dataset as ds
 SHARD_SIZE = 4194304  # (2^22), puts the shard size a bit under 4 GB
 
 
-def take_shard(idxs, embeddings, shards_path, shard_id):
+def parse_args() -> Namespace:
+    parser = ArgumentParser("encode.py", description="Repartitions the embeddings.")
+    parser.add_argument("old_path")
+    parser.add_argument("new_path")
+    parser.add_argument("-s", "--shard-size")
+    return parser.parse_args()
+
+
+def take_shard(
+    idxs: list[pa.Array], embeddings: list[pa.Array], shards_path: str, shard_id: int
+) -> tuple[list[pa.Array], list[pa.Array]]:
     idxs_concat = pa.concat_arrays(idxs)
     embeddings_concat = pa.concat_arrays(embeddings)
 
@@ -41,28 +52,23 @@ def take_shard(idxs, embeddings, shards_path, shard_id):
     return [idxs_concat[SHARD_SIZE:]], [embeddings_concat[SHARD_SIZE:]]
 
 
-try:
-    parts_path = sys.argv[1]
-except IndexError:
-    print("parts_path not provided")
-    exit(0)
+def main():
+    args = parse_args()
 
-try:
-    shards_path = sys.argv[2]
-except IndexError:
-    print("shards_path not provided")
-    exit(0)
+    os.mkdir(args.new_path)
 
-os.mkdir(shards_path)
+    # merge the partial works tables
+    shard_id = 0
+    idxs = []
+    embeddings = []
+    for batch in ds.dataset(args.old_path).to_batches():
+        idxs.append(batch['idxs'])
+        embeddings.append(batch['embeddings'])
+        while len(idxs) >= SHARD_SIZE:
+            idxs, embeddings = take_shard(idxs, embeddings, args.new_path, shard_id)
+            shard_id += 1
+    _, _ = take_shard(idxs, embeddings, args.new_path, shard_id)
 
-# merge the partial works tables
-shard_id = 0
-idxs = []
-embeddings = []
-for batch in ds.dataset(parts_path).to_batches():
-    idxs.append(batch['idxs'])
-    embeddings.append(batch['embeddings'])
-    while len(idxs) >= SHARD_SIZE:
-        idxs, embeddings = take_shard(idxs, embeddings, shards_path, shard_id)
-        shard_id += 1
-_, _ = take_shard(idxs, embeddings, shards_path, shard_id)
+
+if __name__ == "__main__":
+    main()
