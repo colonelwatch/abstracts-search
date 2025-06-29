1e66e1e9c9f09a9b77acb165bfdea9d8d02aa233
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2025-06-28 21:30:18 -0700

    Implement a basic parsing function in env_utils.py for bool-like environment variables

--- utils/env_utils.py
+++ utils/env_utils.py
@@ -14,8 +14,17 @@ def get_env_var[T, U](
     return var
 
 
+def bool_from_str(value: str) -> bool:
+    if value in ["0", "false", "False", "FALSE"]:
+        return False
+    elif value in ["1", "true", "True", "TRUE"]:
+        return True
+    else:
+        raise ValueError(f'invalid value "{value}", expected bool-like')
+
+
 CACHE = get_env_var("ABSEARCH_CACHE", Path, Path.home() / ".cache/abstracts-search")
 MODEL = get_env_var("ABSEARCH_MODEL", default="all-MiniLM-L6-v2")
-TRUST_REMOTE_CODE = get_env_var("ABSEARCH_TRUST_REMOTE_CODE", bool, False)
-FP16 = get_env_var("ABSEARCH_FP16", bool, False)
+TRUST_REMOTE_CODE = get_env_var("ABSEARCH_TRUST_REMOTE_CODE", bool_from_str, False)
+FP16 = get_env_var("ABSEARCH_FP16", bool_from_str, False)
 BF16 = not FP16  # support only bf16 or fp16 for simplicity
